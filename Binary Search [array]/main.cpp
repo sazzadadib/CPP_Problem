1d7b31bcbaf28de04162d372d8173535662cf6a0
Sazzad Hossain <sazzadhossainadib@gmail.com>
2023-04-05 22:30:04 +0600

    Binary Search (Array)

---
+++ Binary Search [array]/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n =10,first =0,last=n;
+    int arr[n] = {1,2,3,4,5,6,8,9,10,11};
+    int mid = (first+last)/2;
+    bool found = false;
+    int s;
+
+    cout << "Enter a Number: ";
+    cin >> s;
+
+
+    while(first<=mid && !found)
+    {
+        if(arr[mid]>s)
+        {
+            last = mid - 1;
+            mid = (first+last)/2;
+        }
+        else if (arr[mid]<s)
+        {
+            first= mid+1;
+            mid = (first+last)/2;
+        }
+        else if(arr[mid]==s)
+        {
+            found = true;
+
+        }
+    }
+
+    if(found)
+    {
+        cout << "Found";
+    }
+    else
+    {
+        cout << "Not Found";
+    }
+
+    return 0;
+}
